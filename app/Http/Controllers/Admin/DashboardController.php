3fb04d14e581cb03235815a77e476d084a5a2c7a
artapamudaid <pamuda.id@gmail.com>
2021-12-29 11:31:09 +0700

    add statistic on dashboard page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Transaction;
+use App\TravelPackage;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $requiest)
     {
-        return view('pages.admin.dashboard');
+        return view(
+            'pages.admin.dashboard',
+            [
+                'travel_packages' => TravelPackage::count(),
+                'transactions' => Transaction::count(),
+                'transactions_pending' => Transaction::where('transaction_status', 'PENDING')->count(),
+                'transactions_success' => Transaction::where('transaction_status', 'SUCCESS')->count(),
+            ]
+        );
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -22,7 +22,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Paket</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $travel_packages }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-hotel fa-2x text-gray-300"></i>
@@ -40,7 +40,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                 Transaksi</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">14</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transactions }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -60,7 +60,9 @@
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">8</div>
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $transactions_pending
+                                        }}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -80,7 +82,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                 Sukses</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transactions_success }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-check fa-2x text-gray-300"></i>
